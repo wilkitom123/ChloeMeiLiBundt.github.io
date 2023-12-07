5796b7165835e938ed037c68d636b9b1f91769e3
Tyler Smith <tylersmith4596@gmail.com>
2023-12-06 22:51:36 -0800

    styled the website as a courtesy to the repo owner

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-window.addEventListener('scroll', function () {
-    var navbar = document.querySelector('.navbar');
-    var scrollPosition = window.scrollY;
+// window.addEventListener('scroll', function () {
+//     var navbar = document.querySelector('.navbar');
+//     var scrollPosition = window.scrollY;
 
-    if (scrollPosition > document.getElementById("container").offsetHeight) {
-        navbar.classList.add('fixed-navbar');
-    } else {
-        navbar.classList.remove('fixed-navbar');
-    }
-});
+//     if (scrollPosition = document.getElementById("hero-img1").offsetHeight) {
+//         navbar.classList.add('fixed-navbar');
+//     } else {
+//         navbar.classList.remove('fixed-navbar');
+//     }
+// });
